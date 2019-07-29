762c528f4dd35b6db6e6c1e92b82d55561abff45
dmheisel <d.m.heisel@gmail.com>
2019-07-28 21:52:50 -0500

    add check if emp is on table, monthly cost display

--- scripts/client.js
+++ scripts/client.js
@@ -7,6 +7,7 @@ function handleReady() {
 	$('#employeeForm').submit(handleSubmit);
 	$('#tableContainer').on('click', '.deleteButton', handleDelete);
 }
+
 function handleDelete() {
 	//remove from employee array
 	let idNumber = $(this)
@@ -22,19 +23,34 @@ function handleDelete() {
 	// el.remove();
 	updateTable();
 }
+
 function handleSubmit() {
 	event.preventDefault(); // prevents refresh/default action of form submission
 
 	let inputFields = $('#employeeForm :input');
 	//selector for finding all input fields
 	let employee = {};
-	inputFields.each(function Employee() {
+	inputFields.each(function() {
 		// console.log($(this).attr('name'), $(this).val());
 		employee[$(this).attr('name')] = $(this).val();
 	});
 
+	let matchingIds = employeeList.filter(
+		emp => emp.employeeID === employee.employeeID
+	);
+
+	if (matchingIds.length > 0) {
+		alert(
+			'Employee ID# already present in table.  Check number and re-submit.'
+		);
+		return;
+	}
+	//validate that employee isn't already on the table by checking ID
+
 	employee.salary = parseInt(employee.salary);
-  employeeList.push(employee); // adds to list arary to contain all employees
+	//convert salary from str to int
+
+	employeeList.push(employee); // adds to list arary to contain all employees
 
 	inputFields.val('');
 	updateTable();
@@ -45,32 +61,43 @@ function updateTable() {
 	$('#employeeTableBody').empty();
 	for (let employee of employeeList) {
 		let html = `<tr>
-                <td class="emp-ID">${employee.employeeID}</td>
-                <td>${employee.firstName}</td>
-                <td>${employee.lastName}</td>
-                <td>${employee.title}</td>
-                <td>${employee.salary}</td>
-                <td>
-								<button
-									type="button"
-									class="deleteButton btn btn-outline-secondary btn-sm"
-								>
-									Delete
-                </button>
-                </td>
-                </tr>`;
+      <td class="emp-ID">${employee.employeeID}</td>
+      <td class="name-first">${employee.firstName}</td>
+      <td class="name-last">${employee.lastName}</td>
+      <td class="emp-title">${employee.title}</td>
+      <td class="emp-salary">${employee.salary}</td>
+      <td>
+				<button
+					type="button"
+					class="deleteButton btn btn-outline-secondary btn-sm"
+					>
+					Delete
+        </button>
+        </td>
+      </tr>`;
 		$('#employeeTableBody').append(html);
 	}
 	calcMonthlyCost();
 }
+
 function calcMonthlyCost() {
 	let annualCost = 0;
 	for (let employee of employeeList) {
 		annualCost += employee.salary;
 	}
 	let monthlyCost = annualCost / 12;
-	$('#totalMonthlyCost').text(monthlyCost);
+
+	$('#totalMonthlyCost').text(
+		`${monthlyCost.toLocaleString('en', {
+			minimumFractionDigits: 2,
+			maximumFractionDigits: 2,
+			style: 'currency',
+			currency: 'USD',
+			currencyDisplay: 'symbol'
+		})}`
+	); // parses cost to readable display in USD
+
 	if (monthlyCost > 20000) {
-		$('#totalMonthlyCost').css('backgroundColor', 'red');
+		$('#totalMonthlyCost').addClass('badge-danger');
 	}
 }
